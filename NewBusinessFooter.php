163dc4f81e4014443d31ad0788deaaf2c9d98809
jaimin <dabgarjaimin6@gmail.com>
2025-04-12 15:13:28 +0530

    new Business footer

---
+++ NewBusinessFooter.php
@@ -0,0 +1,207 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Partnership Firm Registration</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/main.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="row col-md-12 container main-container-2 justify-content-center mt-5 p-3 px-1">
+        <h2 class="text-center mb-4">Proprietorship vs Limited Liability Partnership (LLP) vs Company</h2>
+        <div class="table-responsive table-border-radius table-width">
+            <table class="table table-bordered align-middle table-striped text-grey">
+                <thead class="table-light">
+                    <tr>
+                        <th>Features</th>
+                        <th class="text-primary">Proprietorship</th>
+                        <th class="text-primary">Partnership</th>
+                        <th class="text-primary">LLP</th>
+                        <th class="text-primary">Company</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><strong>Definition</strong></td>
+                        <td>Unregistered type of business entity managed by one single person</td>
+                        <td>A formal agreement between two or more parties to manage and operate a business</td>
+                        <td>A Limited Liability Partnership is a hybrid combination having features similar to a
+                            partnership firm and liabilities similar to a company.</td>
+                        <td>Registered type of entity with limited liability to the owners and shareholders</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Ownership</strong></td>
+                        <td>
+                            Sole Ownership
+                        </td>
+
+                        <td>
+                            Min 2 Partner
+                            <ul>
+                                <li>Max 50 Partners</li>
+                            </ul>
+                        </td>
+                        <td>
+
+                            <a href="#" class="text-decoration-none text-primary">Designated Partners</a>
+
+                        </td>
+                        <td>
+                            <ul>
+                                <li><a href="#" class="text-decoration-none text-primary">Min 2 Directors</a></li>
+                                <li>Min 2 Shareholders</li>
+                                <li>Max 15 Directors</li>
+                                <li>Max 200 Shareholders</li>
+                            </ul>
+                            <ul>
+                                <li>For <a href="#" class="text-decoration-none text-primary">One Person Company</a>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>1 Director</li>
+                                <li>1 Nominee Director</li>
+                            </ul>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td class="text-left">Registration Time</td>
+                        <td colspan="4" class="text-center">7-9 working days</td>
+                    </tr>
+
+                    <tr>
+                        <td class="text-left">Promoter Liability</td>
+                        <td colspan="2" class="text-center">Unlimited Liability</td>
+                        <td colspan="2" class="text-center"><a href="#" class="link-info">Limited Liability</a></td>
+                    </tr>
+
+                    <tr>
+                        <td class="text-left">Documentation</td>
+                        <td class="pt-4 lh-base">
+                            <ul>
+                                <li><a href="#" class="link-info">MSME</a></li>
+
+                                <li><a href="#" class="link-info">GST Registration</a></li>
+                            </ul>
+                        </td>
+
+                        <td><a href="#" class="link-info"> Partnership Deed</a></td>
+
+                        <td class="pt-4 lh-base">
+                            <ul>
+                                <li>LLP Deed</li>
+
+                                <li>Incorporation Certificate</li>
+                            </ul>
+                        </td>
+
+                        <td class="pt-4 lh-base">
+                            <ul>
+                                <li><a href="#" class="link-info">MOA</a></li>
+
+                                <li><a href="#" class="link-info">AOA</a></li>
+                                <li><a href="#" class="link-info">Incorporation Certificate</a></li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Governance</td>
+                        <td>-</td>
+                        <td>Under Partnership Act</td>
+                        <td><a href="#" class="link-info"> Act, 2008</a></td>
+                        <td><a href="#" class="link-info"> Under Companies Act, 2013</a></td>
+                    </tr>
+
+                    <tr>
+                        <td class="text-left">Transferability</td>
+                        <td>Non-Transferable</td>
+                        <td>Transferable if registered under ROF</td>
+                        <td colspan="2" class="text-center"><a href="#" class="link-info">Transferable</a></td>
+                    </tr>
+
+                    <tr>
+                        <td>Compliance Requirements</td>
+                        <td>Income tax filing if turnover is more than Rs.2.5 lakhs</td>
+                        <td><a href="#" class="link-info">ITR 5</a></td>
+                        <td>
+                            <ul>
+                                <li><a href="#" class="link-info">FORM 11</a></li>
+                                <li><a href="#" class="link-info">FORM 8</a></li>
+                                <li><a href="#" class="link-info">ITR 5</a></li>
+                            </ul>
+                        </td>
+                        <td>
+                            <ul>
+                                <li><a href="#" class="link-info">ITR 6</a></li>
+                                <li><a href="#" class="link-info">MCA FILLINGS</a></li>
+                                <li><a href="#" class="link-info">AUDITOR'S APPOINTMENT</a></li>
+                            </ul>
+                            <a href="#" class="link-info">Know More</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <h3 class="text-center my-5">Documents Required For Partnership Firm Registration</h3>
+    <div class="row col-md-12 container main-container-2 mt-5 p-0 bg-grey documents_required_gap">
+        <div class="col-md-6 py-3 ps-3 bg-white border-radius">
+            <div class="border-bottom">
+                <ul class="my-3">
+                    <li>PAN Card</li>
+                    <li>PAN of Partners</li>
+                </ul>
+            </div>
+            <div class="border-bottom align-middle">
+                <ul class="my-3">
+                    <li>Adhar Card</li>
+                    <li>Adhar of Partners</li>
+                </ul>
+            </div>
+            <div class="border-bottom">
+                <ul class="my-3">
+                    <li>Rental agreement</li>
+                </ul>
+            </div>
+            <button class="btn btn-primary float-start my-3">Load More </button>
+        </div>
+
+
+        <div class="col-md-6 p-3 bg-white border-radius">
+            <div class="border-bottom my-3">
+                <span><a href="#" class="link-info">Documents Required for LLP Registration</span>
+                <span class="badge rounded-pill bg-primary float-end badge-border-radius">10</span>
+            </div>
+
+            <div class="border-bottom my-3">
+                <span><a href="#" class="link-info">Documents Required for LLP Registration</span>
+                <span class="badge rounded-pill bg-primary float-end">10</span>
+            </div>
+
+            <div class="border-bottom my-3">
+                <span><a href="#" class="link-info">Documents Required for LLP Registration</span>
+                <span class="badge rounded-pill bg-primary float-end">10</span>
+            </div>
+
+            <div class="border-bottom my-3">
+                <span><a href="#" class="link-info">Documents Required for LLP Registration</span>
+                <span class="badge rounded-pill bg-primary float-end">10</span>
+            </div>
+
+            <div class="border-bottom my-3">
+                <span><a href="#" class="link-info">Documents Required for LLP Registration</span>
+                <span class="badge rounded-pill bg-primary float-end">10</span>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- page.php
+++ page.php
@@ -12,24 +12,22 @@
 <body>
     <?php include 'header.php'; ?>
 
-    <div class="container-fluid mt-5 main-container">
-        <div class="row p-3">
-            <div class="col-md-8 col1 rounded p-3 height-fit-content">
+    <div class="container my-5 main-container">
+        <div class="row p-0 ">
+            <div class="col-md-8 col1 p-3 height-fit-content main-container-border-radius">
                 <div class="row">
-                    <div class="col-md-4 first-card">
+                    <div class="col-md-5 ps-3 first-card p-0">
                         <div class="card border-0">
-                            <img src="assets/img/services.jpg" class="card-img-top" alt="Partnership Registration">
-                            <div class="card-body">
-                                <ul>
-                                    <li>Pan Card</li>
-                                    <li>Aadhar Card</li>
-                                    <li>Rental Agreement</li>
-                                </ul>
+                            <img src="assets/img/services.jpg" class="card-img-top rounded-lg w-90" alt="Partnership Registration">
+                            <div class="card-body p-0 pt-3 d-grid gap-2">   
+                                    <span>Pan Card</span>
+                                    <span>Aadhar Card</span>
+                                    <span>Rental Agreement</span>
                                 <a href="#" class="text-primary">Load More</a>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-8 ps-5">
+                    <div class="col-md-7 ps-5">
                         <div class="card border-0">
                             <h4>Partnership Firm Registration</h4>
                             <p><span class="text-primary fw-bold">4.3</span> 7596 Customers</p>
@@ -85,8 +83,8 @@
 
             </div>
             <div class="col-md-4 ps-5 col2">
-                <div class="card border-0 rounded p-3">
-                    <div class="card-header border-0 ">
+                <div class="card border-0 main-container-border-radius">
+                    <div class="card-header p-5 pt-0  right-card-header border-0 ">
                         <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
                             <path
                                 d="M29.4 8.85A2.48 2.48 0 0 0 27.53 8H14a1 1 0 0 0 0 2h13.53a.47.47 0 0 1 .36.16.48.48 0 0 1 .11.36l-1.45 10a1.71 1.71 0 0 1-1.7 1.48H14.23a1.72 1.72 0 0 1-1.68-1.33L10 8.79l-.5-1.92A3.79 3.79 0 0 0 5.82 4H3a1 1 0 0 0 0 2h2.82a1.8 1.8 0 0 1 1.74 1.36L8 9.21l2.6 11.88A3.72 3.72 0 0 0 14.23 24h10.62a3.74 3.74 0 0 0 3.68-3.16l1.45-10a2.45 2.45 0 0 0-.58-1.99zM16 25h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM25 25h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2z" />
@@ -94,11 +92,12 @@
                         <h4 class="mt-3 text-center "><b>YOUR CART IS EMPTY!</b></h4>
                         <p class="mb-2 text-center">Browse Our Services To Add Some Services In Cart</p>
                     </div>
-                    <div class="card-body text-center">
+                    <div class="card-body pt-4 text-center">
                         <span class=" text-2sm text-gray-700"> Existing User?<a href="#"
                                 class="text-primary ps-3">LOGIN</a></span>
                     </div>
-                    <h5>Get Started</h5>
+
+                <div class="form p-3">
                     <input type="text" class="form-control mb-2" placeholder="Name">
                     <input type="email" class="form-control mb-2" placeholder="Email">
                     <input type="tel" class="form-control mb-2" placeholder="Phone">
@@ -108,18 +107,24 @@
                     </div>
                     <button class="btn btn-primary w-100 mt-2">Get Started</button>
                 </div>
+                
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-8">
-                <div class="card border-0 p-3" id="paragraph">
-                    <h3 class=" text-center">Partnership Firm Registration</h3>
+
+
+        <div class="row col-md-12 container mt-5 p-0">
+            <!-- <div class="col-md-8 main-container-border-radius"> -->
+            <div class="col-md-8 col1 p-3 ps-4 height-fit-content main-container-border-radius">
+
+                <div class="card border-0" id="paragraph">
+                    <h3 class=" text-center mt-2">Partnership Firm Registration</h3>
                     <p class="text-align-justify">partnership firm is a popular choice among entrepreneurs due to its
                         simplicity and flexibility. It
                         allows multiple individuals to come together and combine their resources, skills, and expertise to
                         run a business. Registering your partnership firm is the first step towards formalizing your
                         partnership and ensuring its legal recognition.
-    
+
                         At IndiaFilings, we understand that navigating the intricacies of partnership firm registration
                         process can be daunting. That's why we offer a comprehensive and hassle-free partnership firm
                         registration online service designed to meet your needs with affordable partnership firm
@@ -127,14 +132,23 @@
                         Whether you are a new startup or an existing unregistered partnership looking to formalize your
                         business, our expert team of professionals guides you through the process to register partnership
                         firm.
-    
+
                         Contact us now to learn more and get started on your partnership firm registration journey.</p>
                 </div>
             </div>
-        </div>
-    </div>
+                <div class="col-md-8">
 
-    <?php include 'footer.php'; ?>
+                </div>
+
+
+        </div>
+
+
+        <?php include 'NewBusinessFooter.php'; ?>
+
+        <!-- main-container nu div che -->
+    </div>
+    <!-- main-container nu div che -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
